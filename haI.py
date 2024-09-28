3e6000379dd26b3bd368dd09886715159824d8c8
tronghai180598 <154949100+tronghai180598@users.noreply.github.com>
2024-09-29 02:43:27 +0700

    Update haI.py

--- haI.py
+++ haI.py
@@ -11,11 +11,13 @@ st.write("1 ĐỜI AN YÊN VÀ HẠNH PHÚC")
 
 # Button for a surprise message
 if st.button('EM ẤN NÚT 1'):
-    st.success("Umbala, Hôm nay là sinh nhật cô bé Thiên Bình và hay dỗi của anh." " "
-               "Một ngày hết sức ý nghĩa cho em và cho cả anh nữa." " "
-               "Vì điều kiện không cho phép anh được ở gần em trong ngày tuyệt vời này nên a muốn làm gì đó đặc biệt để gửi tới em")
+    st.success("Umbala, Hôm nay là sinh nhật cô bé Thiên Bình và hay dỗi của anh." 
+               " Một ngày hết sức ý nghĩa cho em và cho cả anh nữa." 
+               " Vì điều kiện không cho phép anh được ở gần em trong ngày tuyệt vời này nên anh muốn làm gì đó đặc biệt để gửi tới em")
+
 if st.button('EM ẤN NÚT 2'):
-    st.image("hoa hong.jfif", caption="Anh ước rằng trong thời gian gần nhất bàn tay chúng chúng ta sẽ thay tay của họ trong ảnh này", use_column_width=True)
+    st.image("hoa_hong.jfif", caption="Anh ước rằng trong thời gian gần nhất bàn tay chúng ta sẽ thay tay của họ trong ảnh này", use_column_width=True)
+
 if st.button('EM ẤN NÚT 3'):
     video_file = open('video_nen.mp4', 'rb')
     video_bytes = video_file.read()
@@ -29,23 +31,17 @@ st.write("HEHE. NGA NGỐ CÓ HÓNG ANH VỀ KHÔNG???")
 st.subheader("🎈 A Little Party Game 🎈")
 if st.button('BẤM VÔ ĐÂY ĐI EM ÊI'):
     st.balloons()  # This will make balloons fall on the screen
-    st.success("NGA NGỐ CÓ ĐỒNG Ý NĂM SAU VÈ DẠM NGÕ VỚI ANH KHÔNG???")
-
-    choice = st.radio("Select an answer:", (st.button('YES'), st.button('NO')))
+    st.success("NGA NGỐ CÓ ĐỒNG Ý NĂM SAU VỀ DẠM NGÕ VỚI ANH KHÔNG???")
 
     # Initialize session state for 'answer' to track user's choice
     if 'answer' not in st.session_state:
         st.session_state.answer = None
 
-    # Buttons for choice between Yes and No
-    if choice == st.button('YES'):
+    # Buttons for choices
+    if st.button('YES'):
         st.success("OK")
         st.balloons()  # This will make balloons fall on the screen
         st.session_state.answer = "Yes"  # Stop asking when Yes is selected
     elif st.button('NO'):
-        st.warning("Again")
-        st.session_state.answer = "No"
-
-    # If 'No' was selected, show the question again
-    if st.session_state.answer == "No":
         st.warning("Chọn lại đi!")
+        st.session_state.answer = "No"
