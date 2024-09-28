3901dd2e098f57679d3b3a6b72f9ac68b89e56a4
tronghai180598 <154949100+tronghai180598@users.noreply.github.com>
2024-09-29 00:49:28 +0700

    Update haI.py

--- haI.py
+++ haI.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="CHÚC MỪNG SINH NHẬT", page_icon="🎉")
 
 # Title for the web page
-st.title("🎉 CHÚC MỪNG SINH NHẬT NGA NGỐ 🎉")
+st.title("🎉🎉 CHÚC MỪNG SINH NHẬT NGA NGỐ 🎉🎉")
 
 # Display the birthday message
 st.write("Wishing you a wonderful birthday filled with joy and happiness!")
