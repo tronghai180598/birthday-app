ffb7c2ec27f47a0efd6296e11472810f9fa269f3
tronghai180598 <154949100+tronghai180598@users.noreply.github.com>
2024-10-02 11:28:20 +0700

    Update haI.py

--- haI.py
+++ haI.py
@@ -13,7 +13,9 @@ st.write("1 ĐỜI AN YÊN VÀ HẠNH PHÚC")
 # Кнопки для сюрприза
 if st.button('EM ẤN NÚT 1'):
     st.image("bánhinhnhat.PNG", caption="CHÚC MỪNG SINH NHẬT NÓC NHÀ", use_column_width=True)
-    st.success("Umbala, hôm nay là sinh nhật cô bé Thiên Bình và hay dỗi của anh.")
+     st.success("Umbala, hôm nay là sinh nhật cô bé Thiên Bình và hay dỗi của anh. "
+               "Một ngày hết sức ý nghĩa cho em và cho cả anh nữa. "
+               "Vì điều kiện không cho phép anh được ở gần em, anh muốn làm gì đó đặc biệt gửi tới em.")
 
 if st.button('EM ẤN NÚT 2'):
     st.image("hoa hong.jfif", caption="Anh ước rằng trong thời gian gần nhất bàn tay chúng ta sẽ thay tay của họ trong ảnh này", use_column_width=True)
